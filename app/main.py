ab84c6082067cdde5b8ec3b39e27577b38ea305e
Cristian Sori <cristiansori2@gmail.com>
2022-06-30 15:47:30 -0400

    sddssd

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 import flask
 import requests
+import atexit
 import requests, ssl
+from apscheduler.schedulers.background import BackgroundScheduler
+
 import time
 import smtplib
 app = flask.Flask(__name__)
-app.config["DEBUG"] = False
-starttime = time.time()
-@app.route('/', methods=['GET'])
-def home():
-    while True:
-     url = 'https://infovisa.ibz.be/ResultFr.aspx?place=HAV&visumnr=17125'
-     response = requests.get(url)
-     if response.text.__contains__("ACCORD"):
+cron = BackgroundScheduler()
+def job_function():
+    print("HOla")
+    url = 'https://infovisa.ibz.be/ResultFr.aspx?place=HAV&visumnr=17125'
+    response = requests.get(url)
+    if response.text.__contains__("ACCORD"):
         port = 465  # For SSL
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
@@ -21,7 +22,7 @@ def home():
                    "SIIIIIIII",
                    "Nos fuimos pa belgica pingaaaaaaaa")
         return "NOS FUIMOS PA BELGICA PINGAAAAAA"
-     elif response.text.__contains__("REJET"):
+    elif response.text.__contains__("En traitement"):
         port = 465  # For SSL
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
@@ -31,7 +32,14 @@ def home():
                    "APELAR",
                    "De pinga hay que Apelar")
         return "De pinga hay que APELAR"
-     elif response.text.__contains__("En traitement"):
-      time.sleep(60.0 - ((time.time() - starttime) % 60.0)) 
-    return "ERROR"
-home()
+    elif response.text.__contains__("ssdsdsdd"):
+      return "sdd"
+app.config["DEBUG"] = False
+job = cron.add_job(job_function, 'interval', minutes=1)
+cron.start()
+job.resume()
+
+#job.remove()
+
+
+
